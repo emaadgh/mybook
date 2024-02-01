d89bac661cd7f83a2282343d6d9ac1415742c439
Emaad Ghorbani <emaadghorbani@gmail.com>
2024-02-01 11:50:07 +0330

    Adds unit tests for PagedList.

---
+++ MyBook.Test/PagedListTests.cs
@@ -0,0 +1,43 @@
+﻿using MyBook.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook.Test
+{
+    public class PagedListTests
+    {
+        PagedList<int> pagedList;
+
+        public PagedListTests()
+        {
+            pagedList = new PagedList<int>(new List<int> { 1, 2, 3 }, 3, 2, 1);
+        }
+
+        [Fact]
+        public void PagedList_HasPrevious_HasPreviousMustReturnTrue()
+        {
+            //Arange 
+
+            //Act
+            bool hasPrevious = pagedList.HasPrevious;
+
+            //Assert
+            Assert.True(hasPrevious);
+        }
+
+        [Fact]
+        public void PagedList_HasNext_HasNextMustReturnTrue()
+        {
+            //Arange 
+
+            //Act
+            bool hasNext = pagedList.HasNext;
+
+            //Assert
+            Assert.True(hasNext);
+        }
+    }
+}
