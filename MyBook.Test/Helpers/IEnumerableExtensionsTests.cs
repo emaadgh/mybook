62afda10db25ba4b906d4a5974a1c2008ad58055
Emaad Ghorbani <emaadghorbani@gmail.com>
2024-02-13 12:00:55 +0330

    Adds unit tests for remained helper classes.

---
+++ MyBook.Test/Helpers/IEnumerableExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using MyBook.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook.Test.Helpers
+{
+    public class IEnumerableExtensionsTests
+    {
+        IEnumerable<TestObject> testObjectSources;
+        public IEnumerableExtensionsTests()
+        {
+            testObjectSources = new List<TestObject>()
+            {
+                new TestObject { Id = 1, Name = "Test1", Description = "Description1" },
+                new TestObject { Id = 2, Name = "Test2", Description = "Description2" },
+                new TestObject { Id = 2, Name = "Test3", Description = "Description3" },
+                new TestObject { Id = 2, Name = "Test4", Description = "Description4" }
+            };
+        }
+
+        [Fact]
+        public void ShapeData_NoFieldsSpecified_MustShapesAllProperties()
+        {
+            // Act
+            var shapedData = testObjectSources.ShapeData(null, null);
+
+            // Assert
+            Assert.Equal(3, ((IDictionary<string, object?>)((List<ExpandoObject>)shapedData).First()).Count);
+        }
+
+        [Fact]
+        public void ShapeData_FieldsSpecified_MustShapesAccordingToFields()
+        {
+            // Act
+            var shapedData = testObjectSources.ShapeData("Id,Name", null);
+
+            // Assert
+            var firstItemDictionary = ((IDictionary<string, object?>)((List<ExpandoObject>)shapedData).First());
+
+            Assert.Equal(2, firstItemDictionary.Count);
+            Assert.Contains("Id", firstItemDictionary.Keys);
+            Assert.Contains("Name", firstItemDictionary.Keys);
+        }
+
+        [Fact]
+        public void ShapeData_RequiredFieldsProvided_MustIncludesRequiredFields()
+        {
+            // Act
+            var shapedData = testObjectSources.ShapeData("Id", "Name");
+
+            // Assert
+            var firstItemDictionary = ((IDictionary<string, object?>)((List<ExpandoObject>)shapedData).First());
+
+            Assert.Equal(2, firstItemDictionary.Count);
+            Assert.Contains("Id", firstItemDictionary.Keys);
+            Assert.Contains("Name", firstItemDictionary.Keys);
+        }
+
+        [Fact]
+        public void ShapeData_FieldDoesNotExist_MustThrowsException()
+        {
+            // Act & Assert
+            Assert.Throws<Exception>(() => testObjectSources.ShapeData("InvalidField", null));
+        }
+
+        [Fact]
+        public void ShapeData_SourceIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            List<TestObject>? nullTestObject = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => nullTestObject.ShapeData("Id", null));
+        }
+
+        private class TestObject
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+        }
+    }
+}
---
+++ MyBook.Test/Helpers/IQueryableExtensionsTests.cs
@@ -0,0 +1,112 @@
+﻿using MyBook.API.Helpers;
+using MyBook.API.Services;
+
+namespace MyBook.Test.Helpers
+{
+    public class IQueryableExtensionsTests
+    {
+        [Fact]
+        public void ApplySort_ValidOrderByAndMappingDictionary_MustAppliesSorting()
+        {
+            // Arrange
+            var source = new List<TestEntity>
+            {
+                new TestEntity { Id = 3, Name = "C" },
+                new TestEntity { Id = 1, Name = "A" },
+                new TestEntity { Id = 2, Name = "B" }
+            }.AsQueryable();
+
+            var mappingDictionary = new Dictionary<string, PropertyMappingValue>
+            {
+                { "Id", new PropertyMappingValue(new[] { "Id" }) },
+                { "Name", new PropertyMappingValue(new[] { "Name" }) }
+            };
+
+            // Act
+            var sortedQuery = source.ApplySort("Name desc", mappingDictionary).ToList();
+
+            // Assert
+            Assert.Equal("C", sortedQuery[0].Name);
+            Assert.Equal("B", sortedQuery[1].Name);
+            Assert.Equal("A", sortedQuery[2].Name);
+        }
+
+        [Fact]
+        public void ApplySort_OrderByNullOrEmpty_MustReturnsOriginalQueryable()
+        {
+            // Arrange
+            var source = new List<TestEntity>().AsQueryable();
+            var mappingDictionary = new Dictionary<string, PropertyMappingValue>();
+
+            // Act
+            var sortedQuery = source.ApplySort("", mappingDictionary);
+
+            // Assert
+            Assert.Same(source, sortedQuery);
+        }
+
+        [Fact]
+        public void ApplySort_NullSource_MustThrowsArgumentNullException()
+        {
+            // Arrange
+            IQueryable<TestEntity>? source = null;
+            var mappingDictionary = new Dictionary<string, PropertyMappingValue>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => source.ApplySort("Name", mappingDictionary));
+        }
+
+        [Fact]
+        public void ApplySort_NullMappingDictionary_MustThrowsArgumentNullException()
+        {
+            // Arrange
+            var source = new List<TestEntity>().AsQueryable();
+            Dictionary<string, PropertyMappingValue>? mappingDictionary = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => source.ApplySort("Name", mappingDictionary));
+        }
+
+        [Fact]
+        public void ApplySort_PropertyNotInMappingDictionary_MustThrowsArgumentException()
+        {
+            // Arrange
+            var source = new List<TestEntity>().AsQueryable();
+            var mappingDictionary = new Dictionary<string, PropertyMappingValue>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => source.ApplySort("InvalidProperty", mappingDictionary));
+        }
+
+        [Fact]
+        public void ApplySort_RevertedSorting_MustRevertsSortOrder()
+        {
+            // Arrange
+            var source = new List<TestEntity>
+            {
+                new TestEntity { Id = 3, Name = "C" },
+                new TestEntity { Id = 1, Name = "A" },
+                new TestEntity { Id = 2, Name = "B" }
+            }.AsQueryable();
+
+            var mappingDictionary = new Dictionary<string, PropertyMappingValue>
+            {
+                { "Id", new PropertyMappingValue(new[] { "Id" }, revert: true) },
+            };
+
+            // Act
+            var sortedQuery = source.ApplySort("Id", mappingDictionary).ToList();
+
+            // Assert
+            Assert.Equal(3, sortedQuery[0].Id);
+            Assert.Equal(2, sortedQuery[1].Id);
+            Assert.Equal(1, sortedQuery[2].Id);
+        }
+
+        private class TestEntity
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+        }
+    }
+}
