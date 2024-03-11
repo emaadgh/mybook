5e47829074810d159621ab1f5c9af92530fd8ee1
Emaad Ghorbani <emaadghorbani@gmail.com>
2024-03-11 11:18:47 +0330

    - Adds tests to postman collection's requests. - Adds new request to postman collection for trying to delete an author with existing books. - Minor fixes in seed data of database and its migrations files.

--- MyBook/DbContexts/MyBookDbContext.cs
+++ MyBook/DbContexts/MyBookDbContext.cs
@@ -36,7 +36,7 @@ namespace MyBook.DbContexts
                 new Book("Harry Potter and the Half-Blood Prince (Harry Potter  #6)")
                 {
                     Id = Guid.Parse("ffba8a54-c990-4862-931e-927b35b3b003"),
-                    ISBN = "439785960",
+                    ISBN = "0747581088",
                     PublicationDate = new DateTime(2006, 9, 16),
                     Publisher = "Scholastic Inc.",
                     AuthorId = Guid.Parse("a4848a8c-49ed-45ec-9ef8-ea3761793db4")
@@ -44,7 +44,7 @@ namespace MyBook.DbContexts
                 new Book("Harry Potter and the Order of the Phoenix (Harry Potter  #5)")
                 {
                     Id = Guid.Parse("9a747b83-82d3-4968-a34f-dab4b5b9ee2b"),
-                    ISBN = "439358078",
+                    ISBN = "0747551006",
                     PublicationDate = new DateTime(2004, 9, 1),
                     Publisher = "Scholastic Inc.",
                     AuthorId = Guid.Parse("a4848a8c-49ed-45ec-9ef8-ea3761793db4")
@@ -52,7 +52,7 @@ namespace MyBook.DbContexts
                 new Book("Harry Potter and the Chamber of Secrets (Harry Potter  #2)")
                 {
                     Id = Guid.Parse("d7d654b5-97e9-4b91-a3c3-580ce3fdc73d"),
-                    ISBN = "439554896",
+                    ISBN = "0747538492",
                     PublicationDate = new DateTime(2003, 1, 11),
                     Publisher = "Scholastic Inc.",
                     AuthorId = Guid.Parse("a4848a8c-49ed-45ec-9ef8-ea3761793db4")
@@ -60,7 +60,7 @@ namespace MyBook.DbContexts
                 new Book("Harry Potter and the Prisoner of Azkaban (Harry Potter  #3)")
                 {
                     Id = Guid.Parse("4599eef6-d41f-45a7-8c75-c9c9172ea62b"),
-                    ISBN = "043965548",
+                    ISBN = "0747542155",
                     PublicationDate = new DateTime(2004, 1, 5),
                     Publisher = "Scholastic Inc.",
                     AuthorId = Guid.Parse("a4848a8c-49ed-45ec-9ef8-ea3761793db4")
@@ -68,7 +68,7 @@ namespace MyBook.DbContexts
                 new Book("The Hitchhiker's Guide to the Galaxy (Hitchhiker's Guide to the Galaxy  #1)")
                 {
                     Id = Guid.Parse("9e1d17e0-9fbd-4242-888a-ea225218ffb3"),
-                    ISBN = "043965548",
+                    ISBN = "0330258648",
                     PublicationDate = new DateTime(2004, 1, 5),
                     Publisher = "Scholastic Inc.",
                     AuthorId = Guid.Parse("d3b05403-79b9-460a-9d5e-a3641fd5a1b2")
---
+++ MyBook/Migrations/20240310072356_Fixes ISBN numbers in seed data.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBook.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixesISBNnumbersinseeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("4599eef6-d41f-45a7-8c75-c9c9172ea62b"),
+                column: "ISBN",
+                value: "0747542155");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("9a747b83-82d3-4968-a34f-dab4b5b9ee2b"),
+                column: "ISBN",
+                value: "0747551006");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("9e1d17e0-9fbd-4242-888a-ea225218ffb3"),
+                column: "ISBN",
+                value: "0330258648");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("d7d654b5-97e9-4b91-a3c3-580ce3fdc73d"),
+                column: "ISBN",
+                value: "0747538492");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("ffba8a54-c990-4862-931e-927b35b3b003"),
+                column: "ISBN",
+                value: "0747581088");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("4599eef6-d41f-45a7-8c75-c9c9172ea62b"),
+                column: "ISBN",
+                value: "043965548");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("9a747b83-82d3-4968-a34f-dab4b5b9ee2b"),
+                column: "ISBN",
+                value: "439358078");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("9e1d17e0-9fbd-4242-888a-ea225218ffb3"),
+                column: "ISBN",
+                value: "043965548");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("d7d654b5-97e9-4b91-a3c3-580ce3fdc73d"),
+                column: "ISBN",
+                value: "439554896");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("ffba8a54-c990-4862-931e-927b35b3b003"),
+                column: "ISBN",
+                value: "439785960");
+        }
+    }
+}
