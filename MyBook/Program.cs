04171c0f62e2a7d59df50a4a0ba0775c919e58e8
Emaad Ghorbani <emaadghorbani@gmail.com>
2024-04-03 10:51:27 +0330

    Adds global error handling for Production environment to the API.

--- MyBook/Program.cs
+++ MyBook/Program.cs
@@ -42,6 +42,17 @@ if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
     app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler(appBuilder =>
+    {
+        appBuilder.Run(async context =>
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+        });
+    });
+}
 
 app.UseHttpsRedirection();
 
